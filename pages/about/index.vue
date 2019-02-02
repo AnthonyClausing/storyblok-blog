5e98a80ad5c137efa152f590e95cd7788da4db74
AnthonyClausing <anthonyclausing@gmail.com>
2019-02-01 20:58:50 -0500

    created post preview component, styled multiple components and views

---
+++ pages/about/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <section id="about-page">
+    <h1>Hello and welcome!</h1>
+    <p>This is the best tech blog you'll ever see in your lifetimes</p>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,56 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        storyblok-blog
-      </h1>
-      <h2 class="subtitle">
-        small blog application using nuxt.js and storyblok
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <section id="posts">
+    <PostPreview
+      v-for="post in posts"
+      :id="post.id"
+      :key="post.id"
+      :title="post.title"
+      :excerpt="post.previewText"
+      :thumbnail-image="post.thumbnailUrl"
+    />
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import PostPreview from '@/components/Blog/PostPreview'
 
 export default {
   components: {
-    Logo
+    PostPreview
+  },
+  data() {
+    return {
+      posts: [
+        {
+          title: 'A new beginning',
+          previewText: 'This will be awesome, don\'t miss it!',
+          thumbnailUrl: 'https://cdn.pixabay.com/photo/2014/05/02/21/49/home-office-336373_960_720.jpg',
+          id: 'a-new-beginning'
+        },
+        {
+          title: 'A second beginning',
+          previewText: 'This will be awesome, don\'t miss it!',
+          thumbnailUrl: 'https://cdn.pixabay.com/photo/2014/05/02/21/49/home-office-336373_960_720.jpg',
+          id: 'a-second-beginning'
+        }
+      ]
+    }
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style scoped>
+  #posts {
+    padding-top: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  @media (min-width: 35rem) {
+    #posts {
+      flex-direction: row;
+    }
+  }
 </style>
